648381007b46f22a57a7aa2baa82be0991be5c67
Elmer Lopez <meme_lopez10@hotmail.com>
2021-02-19 16:50:12 -0600

    Fix indentation

--- main.rb
+++ main.rb
@@ -10,30 +10,29 @@ def bubble_sort(array)
       end
     end
     array
-  end
-  unsorted_array = [10, 2, 8, 1, 0]
-  puts 'Unsorted: ' + unsorted_array.join(', ')
-  puts 'Sorted: ' + bubble_sort(unsorted_array).join(', ')
+end
+unsorted_array = [10, 2, 8, 1, 0]
+puts 'Unsorted: ' + unsorted_array.join(', ')
+puts 'Sorted: ' + bubble_sort(unsorted_array).join(', ')
   
-  def bubble_sort_by(array)
+def bubble_sort_by(array)
     sorted = false
     until sorted
-      sorted = true
-      (array.length - 1).times do |i|
-        if yield(array[i], array[i + 1]).positive?
-          array[i], array[i + 1] = array[i + 1], array[i]
-          sorted = false
+        sorted = true
+        (array.length - 1).times do |i|
+            if yield(array[i], array[i + 1]).positive?
+                array[i], array[i + 1] = array[i + 1], array[i]
+                sorted = false
+            end
         end
-      end
     end
     array
-  end
+end
   
-  unsorted_strings = %w[hi hello hey]
-  puts 'Unsorted-strings: ' + unsorted_strings.join(', ')
-  result = bubble_sort_by(unsorted_strings) do |left, right|
+unsorted_strings = %w[hi hello hey]
+puts 'Unsorted-strings: ' + unsorted_strings.join(', ')
+result = bubble_sort_by(unsorted_strings) do |left, right|
     left.length - right.length
-  end
-  
-  puts 'Sorted-strings: ' + result.join(', ')
-  
+end
+
+puts 'Sorted-strings: ' + result.join(', ')
